8b30584a0a7319c91fe5ef29c79f27c0cc2730c0
Alepha Null <HashTag>
2024-07-11 16:55:31 +0530

    added dark colors

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,30 +1,33 @@
-//'use server';
-//
-//import { createStreamableValue } from 'ai/rsc';
-//import { CoreMessage, streamText } from 'ai';
-//import { openai } from '@ai-sdk/openai';
-//
-//export async function continueConversation(messages: CoreMessage[]) {
-//  const result = await streamText({
-//    model: openai('gpt-4-turbo'),
-//    messages,
-//  });
-//  const data = { test: 'hello' };
-//  const stream = createStreamableValue(result.textStream);
-//  return { message: stream.value, data };
-//}
-
-
-'use server';
-
+'use server'
 import { createStreamableValue } from 'ai/rsc';
 import { CoreMessage, streamText } from 'ai';
 import { openai } from '@ai-sdk/openai';
 
+let prompText = `
+    Act as a personal dietician, chef, and recipe curator. User will ask for recipe suggestions based on certain preferences, Ensure each recipe is tailored according to the provided information, including but not limited to:
+
+    Weight goals (e.g., caloric deficit/surplus)
+    Dietary restrictions (e.g., gluten-free, lactose intolerant)
+    Food restrictions (e.g., halal, kosher)
+    Location-based ingredient filtering (e.g., avocados in India)
+    Budget-based filtering (e.g., expensive cheese vs. budget-friendly)
+
+    Structure your answer as follows, in bullet points:
+
+    Number of people serving: [ ]
+    Number of calories: [ ]
+    Nutritional info: [ ]
+    Dietary Info: (only if asked) [ ]
+    Recipe: [ ]
+
+
+  ` 
+
 export async function continueConversation(messages: CoreMessage[]) {
   'use server';
   const result = await streamText({
-    model: openai('gpt-4-turbo'),
+    model: openai('gpt-4o'),
+    system: prompText,
     messages,
   });
   const data = { test: 'hello' };
